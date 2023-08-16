773f68d977cd422a4174b128192906de62000297
Anas <anasalibinanwar@gmail.com>
2023-08-16 15:02:42 +0500

    Updates

---
+++ precache-manifest.6156852fa95d84b177e11c9eddc30e21.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1db01449cc2685c86a3b",
+    "url": "/reactwebsite/static/css/main.0dcb6071.chunk.css"
+  },
+  {
+    "revision": "1db01449cc2685c86a3b",
+    "url": "/reactwebsite/static/js/main.1db01449.chunk.js"
+  },
+  {
+    "revision": "aaf6072a000430f6380a",
+    "url": "/reactwebsite/static/css/1.4f112f2f.chunk.css"
+  },
+  {
+    "revision": "aaf6072a000430f6380a",
+    "url": "/reactwebsite/static/js/1.aaf6072a.chunk.js"
+  },
+  {
+    "revision": "e5f526a3d253c4624a57",
+    "url": "/reactwebsite/static/js/runtime~main.e5f526a3.js"
+  },
+  {
+    "revision": "7d709935fed59e47a13d6555369a62b3",
+    "url": "/reactwebsite/static/media/0.7d709935.jpg"
+  },
+  {
+    "revision": "8944e655cf5766fac8d14ebae34a1ea0",
+    "url": "/reactwebsite/static/media/people-using-aumented-reality-smartphones_23-2148789355 (Small).8944e655.jpg"
+  },
+  {
+    "revision": "18d6a74370b91880f89da54447ace302",
+    "url": "/reactwebsite/static/media/1.18d6a743.jpg"
+  },
+  {
+    "revision": "c0fc2c1706c69a841e94b1f07ddb6b1e",
+    "url": "/reactwebsite/static/media/2.c0fc2c17.jpg"
+  },
+  {
+    "revision": "838f127d4bb500f68ed548c44b7a0e09",
+    "url": "/reactwebsite/static/media/3.838f127d.jpg"
+  },
+  {
+    "revision": "0dc63169174a8f76821095fc66ab3aa7",
+    "url": "/reactwebsite/static/media/6.0dc63169.jpg"
+  },
+  {
+    "revision": "95c79f197b5f0932456be9807bf29505",
+    "url": "/reactwebsite/static/media/8.95c79f19.jpg"
+  },
+  {
+    "revision": "449335f2c36d7eff703dd261e28f4398",
+    "url": "/reactwebsite/static/media/7.449335f2.jpg"
+  },
+  {
+    "revision": "99118b097559ca9bf510474bf8344a23",
+    "url": "/reactwebsite/index.html"
+  }
+];
